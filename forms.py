b697e480b6fe630784e94f7a27bf2f19227ae7e9
IDGS-805-22001717 <81586@alumnos.utleon.edu.mx>
2026-03-03 20:36:35 -0600

    abc maestros completo 03/03/26

--- forms.py
+++ forms.py
@@ -10,10 +10,36 @@ class UserForm(Form):
         validators.DataRequired(message="El campo es requerido"),
         validators.length(min=4, max=20, message="Ingrese nombre valido")
     ])
-    apaterno=StringField("Apaterno", [
+    apellidos=StringField("Apellidos", [
         validators.DataRequired(message="El campo es requerido")
     ])
     email=EmailField("Correo", [
         validators.DataRequired(message='El correo es requerido'),
         validators.Email(message="Ingresa correo valido")
+    ])
+    telefono=StringField("Teléfono", [
+        validators.DataRequired(message="El campo es requerido")
+    ])
+    
+class UserForm2(Form):
+    matricula=IntegerField('matricula')
+    nombre = StringField('Nombre', [
+        validators.DataRequired(message="El campo nombre es requerido"),
+        validators.Length(min=3, max=50, message="Debe tener entre 3 y 50 caracteres")
+    ])
+
+    apellidos = StringField('Apellidos', [
+        validators.DataRequired(message="El apellido es requerido"),
+        validators.Length(max=50, message="Máximo 50 caracteres")
+    ])
+
+    especialidad = StringField('Especialidad', [
+        validators.DataRequired(message="La especialidad es requerida"),
+        validators.Length(max=50, message="Máximo 50 caracteres")
+    ])
+
+    email = EmailField('Correo', [
+        validators.DataRequired(message="El correo es requerido"),
+        validators.Email(message="Ingrese un correo válido"),
+        validators.Length(max=50, message="Máximo 50 caracteres")
     ])
